b25c3fb68d0e9d5e208dbf272946e3885a6bf453
robhendriks <cyanidenl@icloud.com>
2016-10-03 17:39:59 +0200

    added Google GTest framework

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,21 @@
 
 
 int main() {
-    // Create foo object
-    Foo foo;
+    /*Foo foo;
     foo.setId(1);
     foo.setName("Bar");
     foo.setAge(21);
 
-    // Init the repository (repository factory in the future??)
     FooJsonRepository repository;
 
-    // Save the foo object
     repository.save(foo);
 
-    // Retrieve all the foo objects
     std::vector<Foo> savedFoos = repository.all();
 
-    // Loop through the results
     for(Foo const& savedFoo: savedFoos) {
         std::cout << savedFoo.getName() << " is " << savedFoo.getAge() << " jaar oud";
         std::cout << std::endl;
-    }
+    }*/
 
     return 0;
 }
